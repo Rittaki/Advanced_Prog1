7bdabda36cf0ec36046f7255e231291731b89331
rittaki <com.margo@gmail.com>
2021-11-16 17:03:38 +0200

    added files

---
+++ ex1/anomaly_detection_util.cpp
@@ -0,0 +1,79 @@
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: 337904619 Margarita Kilinkarov 201283033 Gitit Shapira
+ */
+#include <math.h>
+#include <cstdlib>
+#include "anomaly_detection_util.h"
+
+float avg(float* x, int size){
+	float sum=0;
+	for(int i=0;i<size;sum+=x[i],i++);
+	return sum/size;
+}
+
+// returns the variance of X and Y
+float var(float* x, int size){
+	float av=avg(x,size);
+	float sum=0;
+	for(int i=0;i<size;i++){
+		sum+=x[i]*x[i];
+	}
+	return sum/size - av*av;
+}
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size){
+	float sum=0;
+	for(int i=0;i<size;i++){
+		sum+=x[i]*y[i];
+	}
+	sum/=size;
+
+	return sum - avg(x,size)*avg(y,size);
+}
+
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size){
+	return cov(x,y,size)/(sqrt(var(x,size))*sqrt(var(y,size)));
+}
+
+// performs a linear regression and returns the line equation
+Line linear_reg(Point** points, int size){
+	// float x[size],y[size];
+
+	// CHANGED
+	float* x = new float[size];
+	float* y = new float[size];
+	for(int i=0;i<size;i++){
+		x[i]=points[i]->x;
+		y[i]=points[i]->y;
+	}
+	float a=cov(x,y,size)/var(x,size);
+	float b=avg(y,size) - a*(avg(x,size));
+
+	// CHANGED
+	delete x;
+	delete y;
+
+	return Line(a,b);
+}
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size){
+	Line l=linear_reg(points,size);
+	return dev(p,l);
+}
+
+// returns the deviation between point p and the line
+float dev(Point p,Line l){
+	// CHANGED
+	// should be fabs() windows implementaion of abs only accepts int, double
+	return fabs(p.y-l.f(p.x));
+}
+
+
+
+
---
+++ ex2/SimpleAnomalyDetector.cpp
@@ -0,0 +1,90 @@
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: 337904619 Margarita Kilinkarov 201283033 Gitit Shapira
+ */
+#include <iostream>
+#include <iterator>
+#include "SimpleAnomalyDetector.h"
+
+SimpleAnomalyDetector::SimpleAnomalyDetector() {
+	// TODO Auto-generated constructor stub
+}
+
+SimpleAnomalyDetector::~SimpleAnomalyDetector() {
+	// TODO Auto-generated destructor stub
+}
+/**
+ * @brief gets objects of timeseries, check evwry pair of features
+ * and find the correlative ones
+ * @param ts TimeSeries file
+ */
+void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
+	map<string, vector<float>>::iterator itr1, itr2;
+	auto map = ts.getMap();
+	float pearsResult, maxThreshold = 0.9, maxPearson, maxDev;
+	auto featuresNames = ts.getFeatures();
+	int size = featuresNames.size(), vecSize = map.begin()->second.size();
+	string feature1, feature2, maxCorFeature;
+	Line line;
+
+	for (itr1 = map.begin(); itr1 != map.end(); itr1++)
+	{
+		pearsResult = 0;
+		maxPearson = 0;
+		for (itr2 = next(itr1, 1); itr2 != map.end(); itr2++)
+		{
+			pearsResult = fabs(pearson(itr1->second.data(), itr2->second.data(), 
+										itr1->second.size()));
+			if (pearsResult > maxPearson)
+			{
+				maxPearson = pearsResult;
+				maxCorFeature = itr2->first;
+				line = linear_reg(itr1->second.data(), itr2->second.data(), itr1->second.size());
+				maxDev = max_dev(itr1->second.data(), itr2->second.data(), itr1->second.size(), line);
+			}
+		}
+		
+		if (maxPearson > maxThreshold)
+		{
+			correlatedFeatures c;
+			c.feature1 = itr1->first;
+			c.feature2 = maxCorFeature;
+			c.corrlation = maxPearson;
+			c.lin_reg = line;
+			c.threshold = maxDev * 1.2;
+			cf.push_back(c);
+		}
+	}
+}
+/**
+ * @brief detects anomalies and adds an anomaly report
+ * @param ts Timeseries file
+ * @return vector<AnomalyReport>
+ */
+vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
+	vector<AnomalyReport> ar;
+	auto map = ts.getMap();
+	int size;
+	string description, feat1, feat2;
+	Line line;
+	float maxDev;
+	for (auto itr = cf.begin(); itr != cf.end(); itr++){
+		feat1 = itr->feature1;
+		feat2 = itr->feature2;
+		line = itr->lin_reg;
+		maxDev = itr->threshold;
+		size = map.begin()->second.size();
+		for (int i = 0; i < size; i++)
+		{
+			if (dev(Point(map.find(feat1)->second[i], map.find(feat2)->second[i]), line) > maxDev)
+			{
+				description = feat1 + "-" + feat2;
+				ar.push_back(AnomalyReport(description, i + 1));
+			}
+			
+		}
+		
+	}
+	return ar;
+}
